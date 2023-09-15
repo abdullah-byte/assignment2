1c8b0765d44ceb20078fede3d7777f79df6bf358
abdullah-byte <abdullahmumuni70@gmail.com>
2023-09-15 17:02:42 +0100

    added index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+//Difference between Var, Let and Const
+//Var
+//Declarations are global outside the function meaning they are defined throughout the program but are local when declared within a function
+//Let: Variables declared by let cannot be redeclared and must be declared before use
+//Const: Const creates a variable that cannot be reassigned another value 
