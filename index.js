850eaa09651ad77175243d1e56b97332cc2b78c7
abdullah-byte <abdullahmumuni70@gmail.com>
2023-09-16 17:29:33 +0100

    modified index.js

--- index.js
+++ index.js
@@ -3,3 +3,32 @@
 //Declarations are global outside the function meaning they are defined throughout the program but are local when declared within a function
 //Let: Variables declared by let cannot be redeclared and must be declared before use
 //Const: Const creates a variable that cannot be reassigned another value 
+??
+
+//addition
+function addition(a,b){
+   
+    console.log(a + " + " + b + " = " + (a+b))
+}
+console.log("This is for addition")
+addition(1,2)
+addition(2,4)
+
+// functions of add, multiply and minus
+//Multiplication
+function multiply(a,b){
+   
+    console.log(a + " * " + b + " = " + (a*b))
+}
+console.log("This is for Multiplication")
+multiply(1,2)
+multiply(2,4)
+
+//Subtraction
+function sub(a,b){
+   
+    console.log(a + " - " + b + " = " + (a-b))
+}
+console.log("This is for Subtraction")
+sub(2,1)
+sub(4,2)
