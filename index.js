791577d1b2e1411c112963f104d35873bb46ab3a
abdullah-byte <abdullahmumuni70@gmail.com>
2023-09-18 15:35:28 +0100

    modified index.js

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
 //Declarations are global outside the function meaning they are defined throughout the program but are local when declared within a function
 //Let: Variables declared by let cannot be redeclared and must be declared before use
 //Const: Const creates a variable that cannot be reassigned another value 
-??
+function CountryAbv(Full, Abv){
+    
+    console.log("This is " + Full +" in Abbrevation "+ Abv)
+
+}
+
+
+CountryAbv("Austrailia", "AUS");
+CountryAbv("New Zealand", "NZD");
+CountryAbv("Brasil", "BRA");
+CountryAbv("Japan", "JPN");
+CountryAbv("China", "CHN");
 
 //addition
 function addition(a,b){
@@ -32,3 +43,58 @@ function sub(a,b){
 console.log("This is for Subtraction")
 sub(2,1)
 sub(4,2)
+
+
+let Phone ={
+    brand: "Tecno",
+    Color: "Blue",
+    year: 2023,
+    camera: "48mp",
+    picture: function(){
+        console.log("Take selfie");
+    }
+}
+
+console.log(Phone.brand);
+
+Phone.Color = "Red";
+
+console.log(Phone);
+
+Phone.picture()
+
+
+let person = [
+    {
+        gender: 'female',
+        ageBracket: "15-20",
+        school: "queens college",
+        address: {
+            street: "adekunle street",
+            city: "lagos"
+        }
+    },
+    {
+        gender: 'male',
+        ageBracket: "15-20",
+        school: "Kings college",
+        address: {
+            street: "adekunle street",
+            city: "lagos"
+        }
+    },
+    "Adebayo"
+]// access both genders 
+// change city to ogun
+// change adebayo to your name
+
+console.log(person[0].gender);
+console.log(person[1].gender);
+
+person[0].address.city = "Ogun"
+person[1].address.city ="Ogun"
+
+console.log(person);
+
+person[2]= "Abdullah"
+console.log(person);
